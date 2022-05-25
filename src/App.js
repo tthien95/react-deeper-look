18317bfc187299015850f6425c63fb1fb55ad3ab
Thien Trinh <thien.trinh@hcl.com>
2022-05-25 12:54:27 +0700

    useMemo example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import './App.css';
 import Button from './components/UI/Button/Button';
+import DemoOutput from './Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
